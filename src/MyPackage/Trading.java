3dca45e1a058fd1fec918d321378f8eebaf0e27a
ruchi <ruchika.karki@gmail.com>
2023-09-14 18:58:36 +0200

    Trading Branch

--- src/MyPackage/Trading.java
+++ src/MyPackage/Trading.java
@@ -5,7 +5,7 @@ public class Trading {
 	public static void main(String[] args) {
 		
 		System.out.println("This is a test case for Trading Check");
-
+		System.out.println("This is a test case for Trading Branch New");
 	}
 
 }
