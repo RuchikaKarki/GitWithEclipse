d2d24d574df9aa4096657aaedbf149ad28245226
ruchi <ruchika.karki@gmail.com>
2023-09-14 18:56:17 +0200

    Third Commit

--- src/MyPackage/Trading.java
+++ src/MyPackage/Trading.java
@@ -5,6 +5,7 @@ public class Trading {
 	public static void main(String[] args) {
 		
 		System.out.println("This is a test case for Trading Check");
+		System.out.println("This is a 2nd test case for Trading");
 
 	}
 
